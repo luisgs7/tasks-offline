8cefbd2f47324e20c5131a7fe3c798e93ba898f5
luisgs7 <luisgsilva270@gmail.com>
2024-07-05 22:14:30 -0300

    link fonts, install moment package, new component task

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react"
 import { ImageBackground, StyleSheet, Text, View } from "react-native"
 
-import today from '../../assets/imgs/today.jpg'
+import todayImage from '../../assets/imgs/today.jpg'
+
+import moment from "moment"
+import 'moment/locale/pt-br'
+
+import commonStyles from "../commonStyles"
+import Task from "../components/Task"
 
 export default class TaskList extends Component {
     render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
-                <ImageBackground source={today} style={styles.background}>
-
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.tasklist}>
-                    <Text>TaskList</Text>
+                    <Task desc="Comprar carro" estimateAt={new Date()} doneAt={new Date()}/>
+                    <Task desc="Ler Livro" estimateAt={new Date()} doneAt={null}/>
                 </View>
             </View>
         )
@@ -28,4 +39,22 @@ const styles = StyleSheet.create({
     tasklist: {
         flex: 7,
     },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
+    }
 })
