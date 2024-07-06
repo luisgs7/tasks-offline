71701a24dd1b60961fee5c224736ed4da7ab784c
luisgs7 <luisgsilva270@gmail.com>
2024-07-06 14:10:55 -0300

    create task component and style of checkbox item

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,62 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 export default props => {
     return (
-        <View>
-            <Text>{props.desc}</Text>
-            <Text>{props.estimateAt + ""}</Text>
-            <Text>{props.doneAt + ""}</Text>
+        <View style={styles.container}>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Text>{props.desc}</Text>
+                <Text>{props.estimateAt + ""}</Text>
+            </View>
         </View>
     )
 }
+
+function getCheckView(doneAt) {
+    if (doneAt !=null) {
+        return (
+            <View style={styles.done}>
+                <Icon name='check' size={20} color='#FFF'></Icon>
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.pending}></View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    checkContainer: {
+        width: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    pending: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#555',
+    },
+    done: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        backgroundColor: '#4D7031',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
