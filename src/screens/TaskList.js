93d4838f1aafaead8528c45d4f5dd250808c355e
luisgs7 <luisgsilva270@gmail.com>
2024-07-06 15:32:25 -0300

    added icon for filter data done.

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react"
-import { FlatList, ImageBackground, StyleSheet, Text, View } from "react-native"
+import { FlatList, ImageBackground, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
 import todayImage from '../../assets/imgs/today.jpg'
 
 import moment from "moment"
 import 'moment/locale/pt-br'
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 import commonStyles from "../commonStyles"
 import Task from "../components/Task"
 
 export default class TaskList extends Component {
     state = {
+        showDoneTasks: true,
         tasks: [{
             id: Math.random(),
             desc: 'Comprar Livro de React Native',
@@ -22,6 +24,10 @@ export default class TaskList extends Component {
             estimateAt: new Date(),
             doneAt: null,
         }]
+    }
+
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks })
     }
 
     toggleTask = taskId => {
@@ -40,6 +46,12 @@ export default class TaskList extends Component {
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={this.toggleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                            size={20} color={commonStyles.colors.secondary}/>
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subtitle}>{today}</Text>
@@ -86,5 +98,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30,
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
     }
 })
